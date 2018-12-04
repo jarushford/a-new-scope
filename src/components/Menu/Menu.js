7b82ee18fa6a0463c5597cbb8b8cbc06d10bcd8a
Jamie Rushford <jar.rushford@gmail.com>
2018-12-04 11:48:57 -0700

    Add Labels for Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,24 +8,28 @@ export default function Menu({ changePage }) {
           onClick={() => changePage('favorites')}
           className='favorites-planet'
         >
+        <h3>Favorites</h3>
         </div>
         <div className='vehicles-path'>
           <div
             onClick={() => changePage('vehicles')}
             className='vehicles-planet'
           >
+          <h3>Vehicles</h3>
           </div>
           <div className='planets-path'>
             <div
               onClick={() => changePage('planets')}
               className='planets-planet'
             >
+            <h3>Planets</h3>
             </div>
             <div className='people-path'>
               <div
                 onClick={() => changePage('people')}
                 className='people-planet'
               >
+              <h3>People</h3>
               </div>
             </div>
           </div>
