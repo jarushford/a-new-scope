e91a499128f92edfe391e3d55d0a5c68aa4eff1d
Jamie Rushford <jar.rushford@gmail.com>
2018-12-03 17:16:32 -0700

    Add App tests and setup inital App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Landing from '../Landing/Landing'
+import Menu from '../Menu/Menu'
+import Main from '../Main/Main'
+import './app.scss'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 'landing'
+    }
+  }
+
+  changePage = (page) => {
+    this.setState({ currentPage: page })
+  }
+
   render() {
+    const { currentPage } = this.state
     return (
       <div className="App">
-  
+        {renderHelper[currentPage]}
       </div>
-    );
+    )
   }
+}
+
+/*************
+ PRIVATE
+*************/
+const renderHelper = {
+  landing: <Landing />,
+  menu: <Menu />,
+  people: <Main category='people'/>,
+  planets: <Main category='planets'/>,
+  vehicles: <Main category='vehicles'/>,
 }
 
 export default App;
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  const wrapper = shallow(<App />)
+  
+  it('on page load the current page should be "landing"', () => {
+    expect(wrapper.instance().state.currentPage).toEqual('landing')
+  })
+
+  it('should change current page to the value passed into changePage', () => {
+    wrapper.instance().changePage('menu')
+    expect(wrapper.instance().state.currentPage).toEqual('menu')
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
