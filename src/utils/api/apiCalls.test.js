5b3ee425771131c067b01568e25d2a22fa6a56f2
Jake Lauer <jake.lauer27@gmail.com>
2018-12-05 17:10:39 -0700

    Breakout API call for title scroll to separate file and add tests

---
+++ src/utils/api/apiCalls.test.js
@@ -0,0 +1,51 @@
+import * as API from './apiCalls'
+
+describe('API', () => {
+
+  describe('fetchTitleScroll', () => {
+    let mockFilm
+    let mockFilms
+   
+    beforeEach(() => {
+      mockFilm = {
+        title: 'ep1', 
+        release_date: '1995', 
+        opening_crawl: 'starwars'
+      }
+      mockFilms = [mockFilm, mockFilm]
+      window.fetch = jest.fn().mockImplementation(
+        () => Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockFilms)
+        })
+      )
+    })
+
+    it('Should call fetch with correct params', () => {
+      const expected = 'https://swapi.co/api/films/'
+      API.fetchTitleScroll()
+      expect(window.fetch).toHaveBeenCalledWith(expected)
+    })
+
+    it('Should return films if everything is ok', async () => {
+      const expected = mockFilms
+      const result = await API.fetchTitleScroll()
+
+      expect(result).toEqual(expected)
+    })
+
+    it('Should throw error if everything is not ok', async () => {
+      const expectedError = Error('Internal Server Error')
+      window.fetch = jest.fn().mockImplementation(
+        () => Promise.resolve({
+          ok: false,
+          json: () => Promise.resolve(mockFilms)
+        })
+      )
+
+      await expect(API.fetchTitleScroll()).rejects.toEqual(expectedError)
+    })
+
+  })
+
+})
