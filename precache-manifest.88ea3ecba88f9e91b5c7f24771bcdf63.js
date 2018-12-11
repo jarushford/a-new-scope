95fc3c964f7911680646e4f99db472c86bbe8b81
Jake Lauer <jake.lauer27@gmail.com>
2018-12-11 15:08:11 -0700

    Updates

---
+++ precache-manifest.88ea3ecba88f9e91b5c7f24771bcdf63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3482a5b06d6ed1c60505726b46de4a9c",
+    "url": "/a-new-scope/static/media/crosshair_clicked.3482a5b0.svg"
+  },
+  {
+    "revision": "4d243681f613f5eef621303a54d1f88c",
+    "url": "/a-new-scope/static/media/crosshair.4d243681.svg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/a-new-scope/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "0c13f649498cf1c052f504d863076f26",
+    "url": "/a-new-scope/static/media/PathwayGothicOne-Regular.0c13f649.ttf"
+  },
+  {
+    "revision": "02b5d977ae4cba3cd353",
+    "url": "/a-new-scope/static/js/runtime~main.02b5d977.js"
+  },
+  {
+    "revision": "e993c7bc578966560206",
+    "url": "/a-new-scope/static/js/main.e993c7bc.chunk.js"
+  },
+  {
+    "revision": "6f56b47c61b4bb22934f",
+    "url": "/a-new-scope/static/js/1.6f56b47c.chunk.js"
+  },
+  {
+    "revision": "e993c7bc578966560206",
+    "url": "/a-new-scope/static/css/main.439a4469.chunk.css"
+  },
+  {
+    "revision": "8e06e1dea49e2c71e26943ec963f700e",
+    "url": "/a-new-scope/index.html"
+  }
+];
